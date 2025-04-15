6532d547907080396d54cdc0562032eaf7de085f
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-04-15 22:45:01 +0300

    modalBook-sett

--- src/components/LibraryModal/LibraryModal.jsx
+++ src/components/LibraryModal/LibraryModal.jsx
@@ -1,7 +1,31 @@
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { fetchBookDetails } from "../../redux/library/operations";
 
 const LibraryModal = ({ book, onClose }) => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const bookId = book._id; // Отримуємо ID книги безпосередньо з переданого book об'єкта
+
+  const handleStartReading = () => {
+    console.log("Book ID before dispatch:", bookId);
+
+    if (!bookId) {
+      console.error("Book ID is missing or invalid.");
+      return;
+    }
+
+    dispatch(fetchBookDetails(bookId))
+      .then(() => {
+        navigate(`/reading`);
+      })
+      .catch((error) => {
+        console.error("Error fetching book details:", error);
+        // Можна відобразити повідомлення про помилку
+      });
+
+    onClose();
+  };
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
@@ -10,7 +34,7 @@ const LibraryModal = ({ book, onClose }) => {
         <p>{book.author}</p>
         <img src={book.imageUrl} alt={book.title} width="150" />
         <p>Pages: {book.totalPages}</p>
-        <button onClick={() => navigate("/reading")}>Start reading</button>
+        <button onClick={handleStartReading}>Start reading</button>
       </div>
     </div>
   );
